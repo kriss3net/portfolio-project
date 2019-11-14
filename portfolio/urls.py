5ef7b3cf9c3c2c1c7f817609927fe407bd22b7b6
Kristaps <kristaps.paunins@gmail.com>
2019-11-14 15:25:16 +0200

    No working image GET

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,8 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
